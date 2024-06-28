05edb72d280a2cca55d8ba50a9b996c3bf79c9a6
lactofreee <lactofreee@gmail.com>
2024-06-28 21:59:02 +0900

    init project file

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RecoilRoot } from "recoil";
+
+import App from "./App.tsx";
+import "./styles/index.css"
+
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  </React.StrictMode>
+);
